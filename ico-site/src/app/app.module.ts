0aa5ceb83a8c7e2f4f6022e68f1ef905aa71ec82
Xu Cong <xc4330@gmail.com>
2017-12-23 00:35:22 +0800

    Added component template. Header WIP

--- ico-site/src/app/app.module.ts
+++ ico-site/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TimerComponent } from './dashboard/timer/timer.component';
+import { AboutComponent } from './about/about.component';
+import { EventComponent } from './event/event.component';
+import { TeamComponent } from './team/team.component';
+import { HeaderComponent } from './dashboard/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TimerComponent,
+    AboutComponent,
+    EventComponent,
+    TeamComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
