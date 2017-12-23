fd9fda49bee941a008b9a3aac70acee0a0e91d8a
Xu Cong <xc4330@gmail.com>
2017-12-23 19:15:00 +0800

    Carousel WIP

--- ico-site/src/app/carousel/carousel.component.ts
+++ ico-site/src/app/carousel/carousel.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class CarouselComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+   }
 
   ngOnInit() {
   }
