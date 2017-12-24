4eeb2d005d7ddaa39fd494758c70a44b54e3d4af
Edward Loke <edwardlokekc@gmail.com>
2017-12-24 13:30:07 +0800

    [UPDATE] Update footer and header to make use of navlink file for navigation links

--- ico-site/src/app/footer/footer.component.ts
+++ ico-site/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationLink, NavigationLinks } from '../../data/navlink';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  navlinks: NavigationLink[];
+
   constructor() { }
 
   ngOnInit() {
+    this.navlinks = NavigationLinks;
   }
 
 }
