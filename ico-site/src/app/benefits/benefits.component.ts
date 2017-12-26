6dadd872a713062fcd6446ccadb9736c384e8ebf
Edward Loke <edwardlokekc@gmail.com>
2017-12-26 12:12:54 +0800

    [UPDATES][CSS HTML] Update to css and html for benefits to finish up

--- ico-site/src/app/benefits/benefits.component.ts
+++ ico-site/src/app/benefits/benefits.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-benefits',
   templateUrl: './benefits.component.html',
-  styleUrls: ['./benefits.component.css']
+  styleUrls: ['./benefits.component.scss']
 })
 export class BenefitsComponent implements OnInit {
 
