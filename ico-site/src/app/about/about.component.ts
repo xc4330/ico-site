65ce64346712c42d16674615d191bb7f9444b751
Xu Cong <xc4330@gmail.com>
2017-12-28 03:05:21 +0800

    Minor fixes

--- ico-site/src/app/about/about.component.ts
+++ ico-site/src/app/about/about.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() { } 
-  private iPadImg = require("../../assets/icons/iPad.png");
 
   ngOnInit() {
   }
