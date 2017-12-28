bbdb97ecc48c0c8dfa87bb6340fd32c5433cc149
Xu Cong <xc4330@gmail.com>
2017-12-28 23:26:52 +0800

    Adjusted event stage, timer and roadmap

--- ico-site/src/data/roadmapPhases.ts
+++ ico-site/src/data/roadmapPhases.ts
@@ -13,8 +13,8 @@ export const techPhases: Phase[] = [
   export const bizPhases: Phase[] = [
     new Phase('1','','Q3 2016','Idea and founder been handpicked by SGInnovate (a Government initiative)','past', true, false),
     new Phase('2','','June 2017','Founding advisory team joined in August. Received and in discuss with Global names on distributorship and strategic partnership. Top 5 (out of global 22 bidders) in Smart Dubai Government ','past', false, false),
-    new Phase('3','','Oct 2017','Received initial Seed investment. Completed Whitepaper & soft marketing launch. Early bird token sales in December. Government Polytechnic in Singapore in discussion of a 5-campus implementation of our solution and co create a curriculum','past', false, false),
-    new Phase('4','','Feb 2018 (Southeast Asia)','Launch Official token sales. Official partnership campaign for Southeast Asia Region','current', false, false),
+    new Phase('3','','Oct 2017','Received initial Seed investment. Completed Whitepaper & soft marketing launch. Early bird token sales in December. Government Polytechnic in Singapore in discussion of a 5-campus implementation of our solution and co create a curriculum','current', false, false),
+    new Phase('4','','Feb 2018 (Southeast Asia)','Launch Official token sales. Official partnership campaign for Southeast Asia Region','future', false, false),
     new Phase('5','','June 2018','Official partnership campaign for Rest of Asia and UAE Region. Team capabilities enhancement - recruitment for key functions of technology development and business development. Launch Blockchain Lab initiative across Southeast Asia in all established innovation labs, accelerators, co working space, colleges and Universities.','future', false, false),
     new Phase('6','','Future ','Launch Blockchain Lab initiative across rest of Asia and UAE. Sales activities launch in rest of Asia and UAE','future', false, true),
   ]
