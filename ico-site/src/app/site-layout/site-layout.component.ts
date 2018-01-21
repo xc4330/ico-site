3a30c70dd08e9b1b73d618a46056e15fe1f703ee
jon <jon@maroonstudios.com>
2018-01-21 18:09:34 +0800

    Fix scrolling

--- ico-site/src/app/site-layout/site-layout.component.ts
+++ ico-site/src/app/site-layout/site-layout.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable  } from '@angular/core';
+import { ScrollToModule, ScrollToService, ScrollToConfigOptions  } from '@nicky-lenaers/ngx-scroll-to';
 
 @Component({
   selector: 'app-site-layout',
   templateUrl: './site-layout.component.html',
   styleUrls: ['./site-layout.component.css']
 })
-export class SiteLayoutComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+@Injectable()
+export class SiteLayoutComponent implements OnInit{
+  title = 'app';
+  constructor(private _scrollToService: ScrollToService){}
+  
+  public triggerScrollTo(target:string) {
+    console.log(target)
+    target = target.replace(/\s/g,''); 
+    const config: ScrollToConfigOptions = {
+      target: target,
+      offset: -57,
+    };
+    this._scrollToService.scrollTo(config);
   }
 
+  ngOnInit() {
+  }	
 }
