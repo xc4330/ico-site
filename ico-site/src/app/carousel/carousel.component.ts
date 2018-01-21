a240386c2066e8ab11a39930daccb72b18a4d581
Xu Cong <xc4330@gmail.com>
2018-01-21 22:37:09 +0800

    CHN wip

--- ico-site/src/app/carousel/carousel.component.ts
+++ ico-site/src/app/carousel/carousel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-carousel',
@@ -9,7 +10,7 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class CarouselComponent implements OnInit {
 
-  constructor(config: NgbCarouselConfig) {
+  constructor(config: NgbCarouselConfig, private translate: TranslateService) {
     config.interval = 10000;
     config.wrap = false;
     config.keyboard = false;
