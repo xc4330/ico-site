7dad93a28c7362a8827edfe5d02009b3af3fb5fd
jon <jon@maroonstudios.com>
2018-01-24 00:07:40 +0800

    Edits

--- ico-site/src/app/site-layout/site-layout.component.ts
+++ ico-site/src/app/site-layout/site-layout.component.ts
@@ -33,38 +33,37 @@ export class SiteLayoutComponent implements OnInit, AfterViewInit{
       var parentW = $('.abt-carousel').width() || $(window).width();
     }
   
-  var $img = $('.poster-list').find('li').eq(0).find('img');
-  console.log($img);
+    var $img = $('.poster-list').find('li').eq(0).find('img');
 
-  if($img){
-      $img.ready(function(){
-        var ratio = $('.abt-carousel').width() / ($img.width());
-        var num = ($(window).width() - (parentW * 0.9))/2;
-        var imgHeight = Math.floor($img.height() * ratio / 1.8);
-        $('.poster-next-btn').css('right','0px');
-        $('.poster-prev-btn').css('left','0px');
-        console.log($img.height());
-        var carWidth = (parentW * 0.9)  - 20;
-        var carHeight = imgHeight * 0.9;
-        var pWidth = (parentW * 0.9) / 2;
-        var pHeight = imgHeight * 0.9;
+    if($img){
+        $(document).ready(function(){
+          var ratio = $('.abt-carousel').width() / ($img.width());
+          var num = ($(window).width() - (parentW * 0.9))/2;
+          var imgHeight = Math.floor($img.height() * ratio / 1.8);
+          $('.poster-next-btn').css('right','0px');
+          $('.poster-prev-btn').css('left','0px');
+          console.log($img.height());
+          var carWidth = (parentW * 0.9)  - 20;
+          var carHeight = imgHeight * 0.9;
+          var pWidth = (parentW * 0.9) / 2;
+          var pHeight = imgHeight * 0.9;
 
-        $('.tablet').css('height', (carHeight*1.17414248021) + 'px').css('width', (pWidth*1.34) + 'px');
+          $('.tablet').css('height', (carHeight*1.17414248021) + 'px').css('width', (pWidth*1.34) + 'px');
 
-        $('.poster-main').PicCarousel({
-          'width':carWidth,
-          'height': carHeight,
-          'posterWidth': pWidth,
-          'posterHeight': pHeight,
-          'scale':0.8,
-          'speed':1000,
-          'autoPlay':/Mobile/.test(navigator.userAgent) ?  false : true,
-          'delay':2000,
-          'verticalAlign':'top'
-        });
+          $('.poster-main').PicCarousel({
+            'width':carWidth,
+            'height': carHeight,
+            'posterWidth': pWidth,
+            'posterHeight': pHeight,
+            'scale':0.8,
+            'speed':1000,
+            'autoPlay':/Mobile/.test(navigator.userAgent) ?  false : true,
+            'delay':2000,
+            'verticalAlign':'top'
+          });
 
-      }); 
-  }
+        }); 
+    }
     
   }
 }
