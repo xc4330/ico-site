88a1ea44239b683cd0340c2bfa0d5333d5688fc2
Eddy551 <34982812+Eddy551@users.noreply.github.com>
2018-01-25 01:08:37 +0900

    Add files via upload

--- ico-site/src/data/teams.ts
+++ ico-site/src/data/teams.ts
@@ -7,7 +7,7 @@ export const members: Member[] = [
       'Chuang Pei-Han',
       'Singapore/Taiwan/Myanmar', 
       'B. Info system (University of Melbourne, Australia); Finance Tech (Massachusetts Institute of Technology).',
-      'Serial entrepreneur, experience in raising multi-million funding & grew one of his startups to a million dollar generating business. His BPaaS was picked by SGInnovate (government) as the pioneer batch of incubatees. ',
+      'Serial entrepreneur, raised multi-million funding & grew one of his startups to a million dollar generating business. His BPaaS was picked by SGInnovate (gov) as the pioneer batch of incubatees. ',
       'team'
     ),
     new Member(
@@ -16,7 +16,7 @@ export const members: Member[] = [
       'Branson Lee',
       'Singapore/Malaysia/Korea',
       'B.Comp Eng (Nanyang Technology University, Singapore); Finance Tech (Massachusetts Institute of Technology).',
-      'An Ex-Co member of the Singapore Fintech Association heading the Talent Committee, he is also active public speaker for Blockchain & Fintech events. A blockchain consultant for major corps & gov, and worked with C-Suite Exe on various strategic partnerships.',
+      'Ex-Co member of Singapore Fintech Association heading the Talent Committee, an active speaker for Blockchain events & consultant for corps & gov, he worked with C-Suite Exe on various strategic partnerships.      ',
       'team'
     ),
     new Member(
@@ -25,7 +25,7 @@ export const members: Member[] = [
       'Dorel D. Burcea',
       'Singapore/Italy',
       'B. Physics (University Of Bucharest). MBA(University of Liverpool). Member of Institute of International Finance',
-      'Over 16 years in Financial Services Application Development and IT Management. Seasoned in multi-million digital transformation projects. Group CIO at UniCredit Bank; Tech Advisor in blockchain, IoT and FinTech startups.',
+      'Over 16 years in Financial Services Application Development & IT Management, seasoned in multi-million digital transformation projects. Group CIO at UniCredit Bank; Tech Advisor in blockchain, IoT & FinTech startups.',
       'team'
     ),
     new Member(
@@ -34,7 +34,7 @@ export const members: Member[] = [
       'Bruce Lu Yang',
       'Singapore/China',
       'B.Applied Mathematics (Tianjin University), EMBA (Rutgers Business School), Finance Tech (Massachusetts Institute of Technology)',
-      'Over 20 years in software development and technology innovation. Implemented a blockchain business application in production using HyperLedger Fabric V1.0, the first in the region and the world.',
+      'Over 20 years in software development and tech innovation. Implemented a blockchain business application in production using HyperLedger Fabric V1.0, the first in the world.',
       'team'
     ),
     new Member(
@@ -43,7 +43,7 @@ export const members: Member[] = [
       'Eddy Low',
       'Malaysia/Japan',
       'B.Multimedia Design (The One Academy of Malaysia)',
-      'Over 10 years of specialised in strategy and design. He creates better user interface and user experience in design to generate better marketing and sales to the clients such as UOB, OCBC, DBS, Standard Chartered Bank.',
+      'Over 10 years of specialised in strategy & design. He creates better user interface & user experience in design to generate better marketing & sales to the global banking clients.',
       'team'
     ),
     new Member(
@@ -52,7 +52,7 @@ export const members: Member[] = [
       'Edward Loke',
       'Singapore',
       'B.Info System Tech & Design (Singapore University of technology and Design, Singapore)',
-      'Successfully delivered multiple web and mobile applications for big clients from financial institutions and banks. A strong advocate of Agile Development and a Blockchain Enthusiast.',
+      'Delivered multiple applications for big clients from financial institutions & banks. A full stack developer & a advocate of Agile Development; a Blockchain Enthusiast.',
       'team'
     ),
     new Member(
@@ -61,7 +61,7 @@ export const members: Member[] = [
       'Xu Cong',
       'Singapore/China',
       'B.Info system (National University of Singapore, Singapore)',
-      'Senior UX technology consultant with experience in executing multi-million mobile/web engagement with global clients from banking, retail and manufacturing, a Blockchain Enthusiast.',
+      'Senior full stack developer (UX) with experience in delivering multi-million applications with global clients from banking, retail and manufacturing; a Blockchain Enthusiast.',
       'team'
     ),
   ]
@@ -73,7 +73,7 @@ export const members: Member[] = [
       'Lok Choon Hong',
       'Malaysia/Singapore',
       'Mst of Law (University of Cambridge). E/MBA (INSEAD & Tsinghua), Founding president: Assoc of Intellectual Property Entrepreneurs and Organisations (AIPO) & treasurer of Malaysian Business Angel Network (MBAN).',
-      'The founder director of Global IP Southeast Asia Pte Ltd, an IP rights consultancy firm with offices across Asia. He is also actively involved in venture / angel capital investment in IP based companies.',
+      'Founder director of Global IP Southeast Asia Pte Ltd, owns IP rights consultancy firm with offices across Asia. Actively involved in venture/angel capital investment in IP startups.',
       'adv'
     ),
     new Member(
@@ -82,7 +82,7 @@ export const members: Member[] = [
       'Dr. Allen Yeo',
       'Singapore',
       'PhD.Mech Eng (Glasgow University). MBA Scholar (University at Buffalo). Research scholar (Committee of Vice Chancellors & Principals). Assoc prof (Hon. & Adjunct of National University of Singapore)',
-      'A conference speaker & seasoned entrepreneur; founded several successful companies including a multi-million financial tech company which was acquired. He held IP related key positions at government bodies in Singapore.',
+      'A conference speaker & seasoned entrepreneur; founded several successful companies including a multi-million financial tech company which was acquired. He held IP related key positions at government bodies.',
       'adv'
     ),
     new Member(
@@ -100,7 +100,7 @@ export const members: Member[] = [
       'Dr. Yeo Yong-Kee',
       'Singapore',
       'PHD.Electrical & Comp Eng (Georgia Institute of Technology). National Science Scholarship, 8 patents and more than 50 research papers published.',
-      'A research program manager in A*STAR, managed more than $10million in research grants. Appointed by the Singapore government to lead & develop standards for Singapore’s nationwide fiber network.','adv'),
+      'A research program manager in A*STAR, managed more than $10million in research grants. Appointed by Singapore government to lead & develop standards for nationwide fiber network.','adv'),
     new Member(
       '../assets/icons/Team10.png',
       'https://www.linkedin.com/in/george-han-b253a113',
@@ -116,7 +116,7 @@ export const members: Member[] = [
       'Dr. De-Wai (David) Chou',
       'Taiwan',
       'Ph.D. Finance (LeBow School of Business - Drexel University); Finance Tech (Massachusetts Institute of Technology)',
-      'A professor in University & Independent Director for Nanoplus, Inc., expert in finance, investment, corporate governance and robotic trading. He worked closely with Ministry of Science & Technology (Taiwan) for years.',
+      'A professor & Independent Director for Nanoplus, Inc., expert in finance, investment, corporate governance and robotic trading. He worked closely with Ministry of Science & Technology (Taiwan) for years.',
       'adv'
       ),
     new Member
@@ -125,7 +125,7 @@ export const members: Member[] = [
       'Prof. Simon Choi',
       'Greater China/Hong Kong',
       'B.Law (Peking University); Mst.Law (University of London); CPEC & PCLL (University of Hong Kong); Adjunct Lecturer (Curtin University, Nottingham Trent University, The Hong Kong Polytechnic University); Founding partner of Acme Ardent',
-      'An international lawyer, advised more than 10 ICO projects globally in international law, advising & reviewing blockchain regulations in various jurisdictions. With vast Chinese investor network, he is one of most sought after ICO advisors in North Asia.',
+      'An international lawyer, advised more than 10 ICO projects globally in law, advising & reviewing blockchain regulations in various jurisdictions. With vast Chinese investor network, he is one of most sought after ICO advisors in North Asia.',
       'adv'
     ),
     new Member(
@@ -137,4 +137,13 @@ export const members: Member[] = [
       'Active Blockchain influencer and Keynote Speaker. He was the lead M&A negotiator between DEC & Compaq; and China Mobile & Ericsson. With vast investment network across globe and managing over $2B fundings.',
       'adv'
     ),
+    new Member(
+      '../assets/icons/Team16.png',
+      'https://www.linkedin.com/in/jaredpolites/',
+      'Jared Polites',
+      'USA',
+      'B.BA (The university of Texas, Austin); M. Finance (Science Po; Paris)',
+      'Worked on roughly 25 ICOs actively raising over $150 million. Specialised in marketing for high-growth tech and blockchain startups, including a leading Asian VC firm and executing go-to-market strategies for early-stage projects.',
+      'adv'
+      ),
   ]
