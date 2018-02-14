11d004f9529e784cc41a88ed078ecff76f05ea8f
jon <jon@maroonstudios.com>
2018-02-14 23:41:06 +0800

    Change to MITx

--- ico-site/src/data/events.ts
+++ ico-site/src/data/events.ts
@@ -3,8 +3,8 @@ import { Stage } from '../app/event/stage/stage.model';
 export const stages: Stage[] = [
     // new Stage(1,'bonus1','Pre-ICO Platinum Bonus','date1','time','ongoing'),
     new Stage(2,'bonus2','Early-bird Premium Bonus','date2','time','ongoing'),
-    new Stage(3,'bonus3','Early-bird Premium Bonus','date3','time','new'),
+    //new Stage(3,'bonus3','Early-bird Premium Bonus','date3','time','new'),
     new Stage(4,'bonus4','Pre-sales Additional Bonus','date4','time','new'),
-    new Stage(5,'bonus5','Pre-sales Additional Bonus','date5','time','new'),
+    //new Stage(5,'bonus5','Pre-sales Additional Bonus','date5','time','new'),
     new Stage(6,'bonus6','Official Sales','date6','time','new'),
   ]
